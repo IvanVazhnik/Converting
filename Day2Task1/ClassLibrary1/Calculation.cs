d5e7d615ada4da21af96a44f5db8e6e093c24745
Ivan Vazhnik <Ivan_Vazhnik@epam.com>
2014-09-25 15:03:42 +0300

    add project v 0.5

---
+++ Day2Task1/ClassLibrary1/Calculation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Day2Task2
+{
+    public class Calculation
+    {
+        
+                
+        public int NODCalc(int time,params int[] numbers)
+        {
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            int result = Calc(numbers[0], numbers[1]);
+                      
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            Thread.Sleep(time-ts.Milliseconds);
+
+            return result;
+            
+
+        }
+
+        
+        
+        private static int Calc(int a, int b)
+        {
+            while (b != 0)
+            {
+                b = a % (a = b);
+            }
+            return a;
+
+        }
+
+        private static int Calc(params int[] numbers)
+        {
+
+            int a = numbers[0];
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                a = Calc(a, i+1);
+                
+            }
+
+            return a;
+        }
+
+
+        public int GCD(int a, int b)
+        {
+
+            if (a == 0) return b;
+            if (b == 0) return a;
+            if (a == b) return a;
+            if (a == 1 || b == 1) return 1;
+            if ((a % 2 == 0) && (b % 2 == 0)) return 2 * GCD(a / 2, b / 2);
+            if ((a % 2 == 0) && (b % 2 != 0)) return GCD(a / 2, b);
+            if ((a % 2 != 0) && (b % 2 == 0)) return GCD(a, b / 2);
+            return GCD(b,Math.Abs(a - b));
+
+
+        }
+
+        public int GCD(params int[] numbers)
+        {
+            int a = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                a = GCD(i, i + 1);
+
+            }
+
+            return a;
+
+
+        }
+
+
+
+    }
+}
